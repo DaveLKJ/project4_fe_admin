02bad2bce4702848581d92dd30e48dca84ac9dd4
KongJian <kongjian3@hotmail.com>
2024-06-17 23:51:35 +0800

    add: add page css

--- project4_admin/src/pages/Add/Add.jsx
+++ project4_admin/src/pages/Add/Add.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Add.css";
 import { assets } from "../../assets/assets";
 
 const Add = () => {
+  const [image, setImage] = useState(false);
+
   return (
     <div className="add">
       <form className="flex-col">
         <div className="add-img-upload flex-col">
           <p>Upload Image</p>
           <label htmlFor="image">
-            <img src={assets.upload_area} alt="" />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.upload_area}
+              alt=""
+            />
           </label>
-          <input type="file" id="image" hidden required />
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type="file"
+            id="image"
+            hidden
+            required
+          />
         </div>
         <div className="add-product-name flex-col">
           <p>Product name</p>
@@ -22,16 +33,18 @@ const Add = () => {
           <p>Product description</p>
           <textarea type="description" rows="6" placeholder="Description" />
         </div>
-        <div className="add-category flex-col">
-          <p>Product category</p>
-          <select name="category">
-            <option value="Blended">Blended</option>
-            <option value="Single-origin">Single origin</option>
-          </select>
-        </div>
-        <div className="add-price flex-col">
-          <p>Price</p>
-          <input type="nNumber" name="price" placeholder="Price" />
+        <div className="add-product-price">
+          <div className="add-category-price flex-col">
+            <p>Product category</p>
+            <select name="category">
+              <option value="Blended">Blended</option>
+              <option value="Single-origin">Single origin</option>
+            </select>
+          </div>
+          <div className="add-category-price  flex-col">
+            <p>Price</p>
+            <input type="Number" name="price" placeholder="Price" />
+          </div>
         </div>
         <button type="submit" className="add-btn">
           Add
